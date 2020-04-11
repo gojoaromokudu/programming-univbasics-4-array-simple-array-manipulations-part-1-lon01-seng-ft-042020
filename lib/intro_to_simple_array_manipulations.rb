ba48d21dcb047f05ac8bddec8711ae7de8f42437
Gbenga Ojo-Aromokudu <gojoaromokudu@gmail.com>
2020-04-11 22:51:08 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,23 @@
+def using_push(array, new)
+  array.push(new)
+end
+
+def using_unshift(array, new)
+  array.unshift(new)
+end
+
+def using_pop(array)
+  array.pop
+end
+
+def pop_with_args(array)
+  array.pop(2)
+end
+
+def using_shift(array)
+  array.shift
+end
+
+def shift_with_args(array)
+  array.shift(2)
+end
